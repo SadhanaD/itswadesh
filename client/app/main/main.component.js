8689a176f34de219c82c8e47a8c6fccfe401ca6c
itwadesh <itswadesh@gmail.com>
2016-12-19 14:06:55 +0530

    Google map for counties

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -1,40 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
+import counties from './counties';
 
 export class MainController {
-  awesomeThings = [];
-  newThing = '';
-
   /*@ngInject*/
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  $onInit() {
-    this.$http.get('/api/things')
-      .then(response => {
-        this.awesomeThings = response.data;
-        this.socket.syncUpdates('thing', this.awesomeThings);
-      });
-  }
-
-  addThing() {
-    if(this.newThing) {
-      this.$http.post('/api/things', {
-        name: this.newThing
-      });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete(`/api/things/${thing._id}`);
+  constructor() {
+    this.counties = counties;
+    this.county = counties[0];
   }
 }
 
